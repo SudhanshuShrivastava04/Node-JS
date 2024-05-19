4b5affc71feac31a6ccfed85a8a50e83e9698414
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2024-05-19 17:34:03 +0530

    Node-JS revision-02

---
+++ file-system.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+
+//write file Sync
+fs.writeFileSync("./test01.txt", "Test sync file system"); //blocking
+
+//write file Async
+fs.writeFile("./test02.txt", "Test async file system", (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
+
+//read file Async
+const data01 = fs.readFileSync("./test01.txt", "utf-8");
+console.log(data01);
+
+//read file Async
+fs.readFile("./test02.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
+
+// append data sync
+fs.appendFileSync("./test01.txt", `\n  ${new Date().toString()}`);
+
+//copy file sync
+fs.cpSync("./test01.txt", "./copy-of-test01.txt");
--- hello.js
+++ hello.js
@@ -1,2 +1,7 @@
+const {addFn,subFn} = require("./math-module");
+
 console.log("Welcome to Node JS world");
+
+console.log("Sum of 2 and 4 is: ", addFn(2,4));
+console.log("Substraction of 4 and 2 is: ", subFn(4,2));
 
