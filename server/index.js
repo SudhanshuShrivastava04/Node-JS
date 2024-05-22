2386a723e98625bd3bce882c7625895290caa379
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2024-05-22 15:07:44 +0530

    Node-JS revision-05

--- server/index.js
+++ server/index.js
@@ -1,18 +1,26 @@
 import { createServer } from "http";
 import fs from "fs";
+import url from "url";
 
 const myServer = createServer((req, res) => {
+  if (req.url == "/favicon.ico") return res.end();
+
   const log = `${new Date().toLocaleDateString()}: ${
     req.url
   } New request recieved \n`;
 
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
+
   fs.appendFile("log.txt", log, (err, data) => {
-    switch (req.url) {
+    switch (myUrl.pathname) {
       case "/":
         res.end("Hello from server, it's a Home Page");
         break;
       case "/about":
-        res.end("Hello from server, it's a About Page");
+        const userName = myUrl.query.name
+        res.end(`Hello ${userName} from server, it's a About Page`);
+        break;
       default:
         res.end("404 not found");
         break;
